6ddc23a3b0c5e72eaa4e4f675f78b4cccefd390f
ollie <64110522+olliem5@users.noreply.github.com>
2021-02-18 20:12:07 +1100

    b0nk

--- src/main/java/com/olliem5/pace/handler/EventHandler.java
+++ src/main/java/com/olliem5/pace/handler/EventHandler.java
@@ -20,12 +20,11 @@ import java.util.stream.Collectors;
 public class EventHandler {
     private final HashMap<Object, List<Method>> registeredMap = new HashMap<>();
 
-    //Set this to true if you want to see the outputs.
     private boolean debugLogging = false;
 
     public void register(Object object) {
         registeredMap.put(object, Arrays.stream(object.getClass().getDeclaredMethods())
-        .filter(method -> method.isAnnotationPresent(PaceHandler.class))
+                .filter(method -> method.isAnnotationPresent(PaceHandler.class))
                 .sorted(Comparator.comparing(method -> method.getAnnotation(PaceHandler.class).priority()))
                 .collect(Collectors.toList())
         );
@@ -53,15 +52,16 @@ public class EventHandler {
                         method.invoke(object, event);
                     } catch (IllegalAccessException | InvocationTargetException exception) {
                         if (debugLogging) {
-                            Pace.log("Something went wrong when dispatching the pace event '" + event + "' with the era '" + event.getEventEra() + "'.");
+                            Pace.log("Something went wrong when dispatching the pace event '" + event + "' with the era '" + event.getEventEra());
                         }
 
                         exception.printStackTrace();
                     }
-                }));
+                })
+        );
 
         if (debugLogging) {
-            Pace.log("Finished dispatching '" + event + "' with the era '" + event.getEventEra() + "'!");
+            Pace.log("Successfully dispatched '" + event + "' with the era '" + event.getEventEra() + "'!");
         }
     }
 
@@ -78,7 +78,8 @@ public class EventHandler {
 
                         exception.printStackTrace();
                     }
-                }));
+                })
+        );
 
         return event;
     }
