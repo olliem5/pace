87f9179f6652f5abc0ffffe3decaa9505992f758
ollie <64110522+olliem5@users.noreply.github.com>
2021-02-22 22:47:20 +1100

    release 1.3

--- src/main/java/com/olliem5/pace/Pace.java
+++ src/main/java/com/olliem5/pace/Pace.java
@@ -2,7 +2,7 @@ package com.olliem5.pace;
 
 public class Pace {
     public static final String NAME = "Pace";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
     public static final String PREFIX = "[" + NAME + " " + VERSION + "]";
 
     public static void log(String message) {
